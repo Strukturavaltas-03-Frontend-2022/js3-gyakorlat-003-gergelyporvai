5adae8ad57d0e522180f532beb969a63d8ab0361
GergelyPorvai <porvai.gergely@gmail.com>
2022-09-03 00:23:51 +0200

    v1

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,1 +1,7 @@
+const setCookie = (stringToken) => {
+  const expires = new Date(Date.now() + 15 * 60 * 1000);
+  console.log(expires);
+  document.cookie = `token=${stringToken};expires=${expires};path=/`;
+};
 
+export default setCookie;
